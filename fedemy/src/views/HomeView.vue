bb4fc1b401483594e45b102090ead336564e8562
Miguel Angel Barahona <miguelbarahona00@gmail.com>
2022-09-28 21:51:30 -0500

    login

--- fedemy/src/views/HomeView.vue
+++ fedemy/src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="UTB logo" src="../assets/logo-misiontic.png">
+    <Login msg="Bienvenido al portal Envio Fedemy"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Login from '@/components/Login.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    Login
   }
 }
 </script>
