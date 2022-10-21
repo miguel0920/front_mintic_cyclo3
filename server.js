4dd126ee36a36c82f1628899e1125df144721232
Miguel Barahona <mbarahona@tramiti.co>
2022-10-21 16:51:08 -0500

    modify server.js

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
-import express from "express";
-import serveStatic from "serve-static";
-const app = express();
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started '+ port);
 
-app.use(serveStatic(__dirname) + '/dist');
+// import express from "express";
+// import serveStatic from "serve-static";
+// const app = express();
 
-app.all('*', (req, res) => {
-    res.status(200).sendFile(__dirname + + '/dist/index.html');
-});
+// app.use(serveStatic(__dirname) + '/dist');
 
-app.listen(process.env.PORT || 8080);
+// app.all('*', (req, res) => {
+//     res.status(200).sendFile(__dirname + + '/dist/index.html');
+// });
+
+// app.listen(process.env.PORT || 8080);
--- src/server.js
+++ src/server.js
@@ -1,21 +1,21 @@
-// var express = require('express');
-// var path = require('path');
-// var serveStatic = require('serve-static');
-// app = express();
-// app.use(serveStatic(__dirname + "/dist"));
-// var port = process.env.PORT || 5000;
-// app.listen(port);
-// console.log('server started '+ port);
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started '+ port);
 
 
-import express from "express";
-import serveStatic from "serve-static";
-const app = express();
+// import express from "express";
+// import serveStatic from "serve-static";
+// const app = express();
 
-app.use(serveStatic(__dirname) + '/dist');
+// app.use(serveStatic(__dirname) + '/dist');
 
-app.all('*', (req, res) => {
-    res.status(200).sendFile(__dirname + + '/dist/index.html');
-});
+// app.all('*', (req, res) => {
+//     res.status(200).sendFile(__dirname + + '/dist/index.html');
+// });
 
-app.listen(process.env.PORT || 8080);
+// app.listen(process.env.PORT || 8080);
