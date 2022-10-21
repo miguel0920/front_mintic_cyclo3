4acfb6538c81e4b0c26edf075677c587ca64f055
Miguel Barahona <mbarahona@tramiti.co>
2022-10-21 16:33:06 -0500

    modify server.js and package.json

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started '+ port);
+import express from "express";
+import serveStatic from "serve-static";
+const app = express();
+
+app.use(serveStatic(__dirname) + '/dist');
+
+app.all('*', (req, res) => {
+    res.status(200).sendFile(__dirname + + '/dist/index.html');
+});
+
+app.listen(process.env.PORT || 8080);
