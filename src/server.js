6cf65aec314d832b0c212ca623f7e82c88e8535c
Miguel Angel Barahona <miguelbarahona00@gmail.com>
2022-10-21 20:23:58 -0500

    modify files

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ var serveStatic = require('serve-static');
 var app = express();
 var port = process.env.PORT || 5000;
 app.listen(port);
-console.log('starting project at ' + port);
+console.log('starting project at 1' + port);
 app.engine('pug', require('pug').__express);
 app.set('views', '/');
 app.set('view engine', 'pug');
@@ -23,7 +23,8 @@ app.set('view engine', 'pug');
 app.use(serveStatic(path.join(__dirname, '/')));
 //Return the index for any other GET request
 app.get('/*', function (req, res) {
-    res.sendFile('index.html', {root: path.join(__dirname, '/')});
+    const index = path.join(__dirname, 'build', 'index.html');
+    res.sendFile(index);
 });
 
 console.log('Miguel 3');
