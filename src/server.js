527ed20b614a8281da36b63c8c0a0e699a9bf471
Miguel Angel Barahona <miguelbarahona00@gmail.com>
2022-10-21 19:08:52 -0500

    modify Procfile and server.js

--- src/server.js
+++ src/server.js
@@ -1,31 +1,10 @@
 const express = require('express');
 const path = require('path');
-//const serveStatic = require("serve-static");
-const history = require('connect-history-api-fallback');
+const serveStatic = require("serve-static");
 
-const app = express();
-
-const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
-
-app.use(staticFileMiddleware);
-app.use(history({
-  disableDotRule: true,
-  verbose: true
-}));
-app.use(staticFileMiddleware);
-
-app.get('/', function (req, res) {
-  res.render(path.join(__dirname + '/dist/index.html'));
-});
-
-var server = app.listen(process.env.PORT || 8080, function () {
-  var port = server.address().port;
+app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
   console.log("App now running on port", port);
 });
-
-// app = express();
-// app.use(serveStatic(path.join(__dirname, 'dist')));
-// const port = process.env.PORT || 8080;
-// app.listen(port, () => {
-//     console.log(port);
-// });
